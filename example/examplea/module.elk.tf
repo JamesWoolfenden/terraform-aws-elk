37b708b15481532148772b1b1deafa2a8e84f6b5
James Woolfenden <james.woolfenden@gmail.com>
2026-05-03 19:37:02 +0100

    fix: update terraform-http-ip to response_body fix commit

--- example/examplea/module.elk.tf
+++ example/examplea/module.elk.tf
@@ -8,8 +8,6 @@ module "elk" {
   subnet_public  = data.aws_subnet_ids.public
   vpc_id         = data.aws_vpc.vpc.id
 }
-
 module "ip" {
-  source  = "JamesWoolfenden/ip/http"
-  version = "0.3.7"
+  source = "git::https://github.com/JamesWoolfenden/terraform-http-ip.git?ref=5769331633debca683a81a38470083a0abd39049"
 }
